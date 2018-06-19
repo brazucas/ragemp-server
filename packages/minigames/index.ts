a7aecf1ddbc3574f129b351f54c7913c480995e1
Pedro Papadópolis <pplp93@gmail.com>
2018-06-19 10:12:26 -0300

    chore(index): utilizando constantes para as callbacks

--- packages/minigames/index.ts
+++ packages/minigames/index.ts
@@ -5,10 +5,13 @@ import {PlayerJoinHandler} from "./handler/playerJoin";
 import {PlayerQuitHandler} from "./handler/playerQuit";
 import {PlayerChatHandler} from "./handler/playerDeath";
 import {PlayerDeathHandler} from "./handler/playerChat";
+import EventKey = RageEnums.EventKey;
 
-mp.events.add("playerJoin", PlayerJoinHandler);
-mp.events.add("playerQuit", PlayerQuitHandler);
-mp.events.add("playerChat", PlayerChatHandler);
-mp.events.add("playerDeath", PlayerDeathHandler);
+declare const mp: Mp;
+
+mp.events.add(EventKey.PLAYER_JOIN, PlayerJoinHandler);
+mp.events.add(EventKey.PLAYER_QUIT, PlayerQuitHandler);
+mp.events.add(EventKey.PLAYER_CHAT, PlayerChatHandler);
+mp.events.add(EventKey.PLAYER_DEATH, PlayerDeathHandler);
 
 let minigames = new Minigames();
