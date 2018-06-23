9d387a30a0efbc349e08940d70a6ecaa47836b32
Pedro Papadópolis <pplp93@gmail.com>
2018-06-23 13:57:07 -0300

    chore(db): configuração do sequelize e model de teste

--- common/database/database.js
+++ common/database/database.js
@@ -2,17 +2,34 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var sequelize_typescript_1 = require("sequelize-typescript");
 var environment_1 = require("../environment");
+var SequelizeOrigin = require("sequelize");
+var Rx_1 = require("rxjs/Rx");
+require("rxjs/add/observable/of");
 var Database = (function () {
     function Database() {
+        var Op = SequelizeOrigin.Op;
         this.handler = new sequelize_typescript_1.Sequelize({
+            host: environment_1.environment.database_host,
+            port: environment_1.environment.database_port,
             database: environment_1.environment.database_name,
-            dialect: 'sqlite',
+            pool: {
+                max: 5,
+                min: 0,
+                idle: 10000
+            },
+            operatorsAliases: Op,
+            dialect: 'mysql',
             username: environment_1.environment.database_username,
             password: environment_1.environment.database_password,
-            storage: './database.sqlite',
             modelPaths: [__dirname + '/models']
         });
     }
+    Database.prototype.sync = function () {
+        return Rx_1.Observable.of(this.handler.sync());
+    };
+    Database.prototype.authenticate = function () {
+        return Rx_1.Observable.of(this.handler.authenticate());
+    };
     return Database;
 }());
 exports.Database = Database;
--- common/database/database.ts
+++ common/database/database.ts
@@ -1,17 +1,37 @@
-import {Sequelize} from 'sequelize-typescript';
+import {Sequelize} from "sequelize-typescript";
 import {environment} from "../environment";
+import * as SequelizeOrigin from "sequelize";
+import {Observable} from "rxjs/Rx";
+import 'rxjs/add/observable/of';
 
 export class Database {
-  public handler: Sequelize;
+  public handler: any;
 
   constructor() {
+    const Op: any = SequelizeOrigin.Op;
+
     this.handler = new Sequelize({
+      host: environment.database_host,
+      port: environment.database_port,
       database: environment.database_name,
-      dialect: 'sqlite',
+      pool: {
+        max: 5,
+        min: 0,
+        idle: 10000
+      },
+      operatorsAliases: Op,
+      dialect: 'mysql',
       username: environment.database_username,
       password: environment.database_password,
-      storage: './database.sqlite',
       modelPaths: [__dirname + '/models']
     });
+  }
+
+  public sync(): Observable<any> {
+    return Observable.of(this.handler.sync());
+  }
+
+  public authenticate(): Observable<any> {
+    return Observable.of(this.handler.authenticate());
   }
 }
