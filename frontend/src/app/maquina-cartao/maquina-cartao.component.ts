dad4386ace957dac79de430983996388e5377a8a
Pedro Papadópolis <pplp93@gmail.com>
2019-01-25 09:50:07 -0200

    feat(frontend): página de máquina de cartão

---
+++ frontend/src/app/maquina-cartao/maquina-cartao.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-maquina-cartao',
+  templateUrl: './maquina-cartao.component.html',
+  styleUrls: ['./maquina-cartao.component.scss']
+})
+export class MaquinaCartaoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
