a0dda7d1177de6f060113e04273969542865c915
Pedro Papadópolis <pplp93@gmail.com>
2019-05-03 20:25:02 -0300

    chore(*): index.js

---
+++ client_packages/browser/index.js
@@ -0,0 +1,6 @@
+'use strict';
+mp.events.add('mostrarNavegador', function () {
+    mp.browsers["new"]('package://browser/index.html#/login');
+    mp.gui.cursor.visible = true;
+    mp.gui.chat.push('Mostrando navegador');
+});
