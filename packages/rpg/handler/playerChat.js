3daae2b0ee5cf951f3b590213d65ee1f6d452433
Pedro Papadópolis <pplp93@gmail.com>
2019-05-02 22:44:26 -0300

    fix(*): player chat

--- packages/rpg/handler/playerChat.js
+++ packages/rpg/handler/playerChat.js
@@ -2,6 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 function PlayerChatHandler(player, text) {
     console.log('>>> Chat ', player, text);
+    mp.players.broadcast(`!{#FFFF00}${player.name}: !{#FFFFFF}${text}`);
 }
 exports.PlayerChatHandler = PlayerChatHandler;
 //# sourceMappingURL=playerChat.js.map
--- packages/rpg/handler/playerChat.ts
+++ packages/rpg/handler/playerChat.ts
@@ -1,3 +1,5 @@
 export function PlayerChatHandler (player: PlayerMp, text: string) {
   console.log('>>> Chat ', player, text);
+
+  mp.players.broadcast(`!{#FFFF00}${player.name}: !{#FFFFFF}${text}`);
 }
