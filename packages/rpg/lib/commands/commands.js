44937cff193552ff12dccd3ed7c2d64543daf6c7
Pedro Papadópolis <pplp93@gmail.com>
2019-05-02 22:40:23 -0300

    feat(*): ajustes no comando /darArma

--- packages/rpg/lib/commands/commands.js
+++ packages/rpg/lib/commands/commands.js
@@ -3,7 +3,10 @@ Object.defineProperty(exports, "__esModule", { value: true });
 class Comandos {
     static darArma(player, weaponHash) {
         console.debug(`[COMANDOS - darArma] Dando arma ${weaponHash} para o jogador ${player.name}`);
-        player.giveWeapon(mp.joaat(weaponHash), 1000);
+        const asset = mp.joaat(weaponHash);
+        console.log('>>>> asset ', asset);
+        player.giveWeapon(asset, 1000);
+        player.notify(`Arma ${weaponHash} recebida!`);
     }
 }
 exports.Comandos = Comandos;
--- packages/rpg/lib/commands/commands.ts
+++ packages/rpg/lib/commands/commands.ts
@@ -2,6 +2,12 @@ export class Comandos {
   public static darArma(player: PlayerMp, weaponHash: string) {
     console.debug(`[COMANDOS - darArma] Dando arma ${weaponHash} para o jogador ${player.name}`);
 
-    player.giveWeapon(mp.joaat(weaponHash), 1000);
+    const asset = mp.joaat(weaponHash);
+
+    console.log('>>>> asset ', asset);
+
+    player.giveWeapon(asset, 1000);
+
+    player.notify(`Arma ${weaponHash} recebida!`);
   }
 }
