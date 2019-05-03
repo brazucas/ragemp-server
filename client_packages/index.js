e96833a89cf8f2b97f64b0405a060f0cb7463cd3
Pedro Papadópolis <pplp93@gmail.com>
2019-05-03 11:05:24 -0300

    feat(browser): redirecionando para tela de login

--- client_packages/index.js
+++ client_packages/index.js
@@ -1,6 +1,11 @@
 mp.events.add('mostrarNavegador', () => {
-    mp.browsers.new('package://browser/index.html');
+    const browser = mp.browsers.new('package://browser/index.html');
     mp.gui.cursor.visible = true;
     mp.gui.chat.push('Mostrando navegador');
+    browser.execute("window.location = '/login'");
+    setTimeout(() => {
+        mp.gui.cursor.visible = false;
+        browser.destroy();
+    }, 8000);
 });
 //# sourceMappingURL=index.js.map
--- client_packages/index.ts
+++ client_packages/index.ts
@@ -1,5 +1,11 @@
 mp.events.add('mostrarNavegador', () => {
-  (mp as any).browsers.new('package://browser/index.html');
+  const browser = (mp as any).browsers.new('package://browser/index.html');
   (mp as any).gui.cursor.visible = true;
   (mp as any).gui.chat.push('Mostrando navegador');
+  browser.execute("window.location = '/login'");
+
+  setTimeout(() => {
+    (mp as any).gui.cursor.visible = false;
+    browser.destroy();
+  }, 8000);
 });
