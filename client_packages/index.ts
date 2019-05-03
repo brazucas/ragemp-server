d752c1689ce4870f55a8798720073d3989110e58
Pedro Papadópolis <pplp93@gmail.com>
2019-05-03 01:48:37 -0300

    feat(*): browser

--- client_packages/index.ts
+++ client_packages/index.ts
@@ -1,4 +1,4 @@
 mp.events.add('mostrarNavegador', () => {
-  (mp as any).browsers.new('package://brazucas-browser/dist/index.html');
+  (mp as any).browsers.new('package://brazucas-browser/dist/brazucas-browser/index.html');
   (mp as any).gui.chat.push('Mostrando navegador');
 });
