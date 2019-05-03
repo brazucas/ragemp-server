e6c130e60ef50b16ccd80f1172a00e1f5f46e77c
Pedro Papadópolis <pplp93@gmail.com>
2019-05-03 16:05:57 -0300

    feat(*): build do browser

--- client_packages/browser/index.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-mp.events.add('mostrarNavegador', function () {
-    mp.browsers["new"]('package://browser/index.html#/login');
-    mp.gui.cursor.visible = true;
-    mp.gui.chat.push('Mostrando navegador');
-});
