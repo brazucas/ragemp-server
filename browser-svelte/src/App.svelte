4bc4e5cc6dcdaaa96e7dfb28e25790534a5acdfd
Pedro Papadópolis <pplp93@gmail.com>
2020-08-08 22:53:22 -0300

    feat(svelte): svelte router config #24

--- browser-svelte/src/App.svelte
+++ browser-svelte/src/App.svelte
@@ -1,30 +1,12 @@
 <script>
-	export let name;
+    import Router from 'svelte-spa-router';
+    import routes from './routes';
+
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
+<body>
+<Router {routes}/>
+</body>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
 </style>
---
+++ browser-svelte/src/Login.svelte
@@ -0,0 +1,9 @@
+<script>
+</script>
+
+<main>
+    <h1>Login</h1>
+</main>
+
+<style>
+</style>
